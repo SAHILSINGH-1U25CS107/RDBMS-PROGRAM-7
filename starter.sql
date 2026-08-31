366522b59e6b33f20dc8b31721d661a1fdb8f524
SAHILSINGH-1U25CS107 <sahilto482000@gmail.com>
2026-08-31 11:27:37 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,9 +1,9 @@
-create database college13;
-use college13;
+create database college14;
+use college14;
 
 create table marksheet(RollNo integer(5) primary key,Name varchar(20),Department varchar(10),marks integer(5));
 desc marksheet;
-select*from student
+
 insert marksheet values("1","Arun","CSE","85"),("2","Divya","IT","78"),("3","Karthik","CSE","92"),("4","Nisha","ECE","67"),("5","Rahul","IT","88");
 
 select * from marksheet where marks>80 order by marks DESC;
