b38d9fdc5026dbc460912261ee2bf3d0e8e5d990
SAHILSINGH-1U25CS107 <sahilto482000@gmail.com>
2026-08-31 11:31:17 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,7 +1,7 @@
 create database college14;
 use college14;
 
-create table marksheet(RollNo integer(5) primary key,Name varchar(20),Department varchar(10),marks integer(5));
+create table marksheet(RollNo integer(5) primary key,Name varchar(10),Department varchar(10),marks integer(5));
 desc marksheet;
 
 insert marksheet values("1","Arun","CSE","85"),("2","Divya","IT","78"),("3","Karthik","CSE","92"),("4","Nisha","ECE","67"),("5","Rahul","IT","88");
